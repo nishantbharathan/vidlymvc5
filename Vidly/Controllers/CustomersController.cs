beb418b33d6c4cfba13919f118cdf86c6f874205
iter\bharatn <nishant_bharathan@silvertouch.com>
2016-07-22 13:35:54 +0530

    index on master: 96b1ba1 Initial commit to add default .gitIgnore and .gitAttribute files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; 
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //
+        // GET: /Customer/
+        public ActionResult Index()               
+        {
+            var Customers = GetCustomers();
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.ID == id);
+
+            if (customer == null)
+                return new HttpNotFoundResult();
+
+            return View(customer);
+
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+
+            return new List<Customer>
+            {
+
+                new Customer() {ID=1,Name="Sachin"},
+                new Customer() {ID=2,Name="Rohit"}
+            };
+        }
+	}
+}
